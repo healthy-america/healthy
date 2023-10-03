135a4baeef9a621945818653b7c3b99011d5f158
Brayan Sanchez <bsanchez@aventi.co>
2023-10-03 09:18:29 -0500

    MHA-389 | Fix helper that gets the discount

--- app/code/Aventi/Checkout/Helper/Discount.php
+++ app/code/Aventi/Checkout/Helper/Discount.php
@@ -4,44 +4,59 @@ declare(strict_types=1);
 
 namespace Aventi\Checkout\Helper;
 
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Psr\Log\LoggerInterface;
+
 class Discount
 {
     /**
      * @var \Magento\Checkout\Model\Session
      */
-    protected \Magento\Checkout\Model\Session $checkoutSession;
-
-    /**
-     * @var \Magento\Checkout\Helper\Data
-     */
-    protected \Magento\Checkout\Helper\Data $checkoutHelper;
+    private \Magento\Checkout\Model\Session $checkoutSession;
 
     /**
      * @var \Magento\Quote\Model\Quote|\Magento\Quote\Api\Data\CartInterface
      */
-    protected \Magento\Quote\Model\Quote|\Magento\Quote\Api\Data\CartInterface $quote;
+    private \Magento\Quote\Model\Quote|\Magento\Quote\Api\Data\CartInterface $quote;
+
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
 
     /**
      * @param \Magento\Checkout\Model\Session $checkoutSession
      * @param \Magento\Checkout\Helper\Data $checkoutHelper
      * @param \Magento\Quote\Model\Quote $quote
+     * @param LoggerInterface $logger
      */
     public function __construct(
         \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Checkout\Helper\Data $checkoutHelper,
-        \Magento\Quote\Model\Quote $quote
+        \Magento\Quote\Model\Quote $quote,
+        LoggerInterface $logger
     ) {
         $this->checkoutSession = $checkoutSession;
-        $this->checkoutHelper = $checkoutHelper;
         $this->quote = $quote;
+        $this->logger = $logger;
     }
 
     /**
      * @return \Magento\Quote\Api\Data\CartInterface|\Magento\Quote\Model\Quote
      */
-    public function getQuote(): \Magento\Quote\Api\Data\CartInterface|\Magento\Quote\Model\Quote
+    public function getQuote()
     {
-        return $this->quote;
+        $quote = $this->quote;
+
+        if (count($quote->getAllVisibleItems()) === 0) {
+            try {
+                return $this->checkoutSession->getQuote();
+            } catch (NoSuchEntityException|LocalizedException $e) {
+                $this->logger->error('There was an error getting the quote: ' . $e->getMessage());
+            }
+        } else {
+            return $quote;
+        }
     }
 
     /**
@@ -53,11 +68,15 @@ class Discount
     {
         $discountAmount = 0;
         $quote = $this->getQuote();
-        foreach ($quote->getAllVisibleItems() as $item) {
+
+        foreach ($quote->getItemsCollection() as $item) {
             $discount = $item->getProduct()->getPrice() - $item->getPrice();
             $discount *= $item->getQty();
             $discountAmount += $discount;
         }
-        return ($discountAmount < 0 ? 0 : $discountAmount);
+
+        $this->logger->debug('Discount amount: ' . $discountAmount);
+
+        return (max($discountAmount, 0));
     }
 }
