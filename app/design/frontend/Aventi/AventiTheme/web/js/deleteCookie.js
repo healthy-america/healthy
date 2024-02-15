c97706bac39d7563ac418b4dcdbf4ec3f6bcfb34
cfernandez <cfernandez@aventi.com.co>
2024-02-15 09:30:09 -0500

    Fix home redirect error and return to healthy with delete cookie

--- app/design/frontend/Aventi/AventiTheme/web/js/deleteCookie.js
+++ app/design/frontend/Aventi/AventiTheme/web/js/deleteCookie.js
@@ -3,18 +3,18 @@ require([
     'mage/cookies'
 ], function ($) {
 
-    $(document).ready(function () {
+    $(document).ready(async function () {
         const prehome = $('main#prehome'),
               body = $('body'),
               loader = $('div.loader'),
               homebody = $('.page-wrapper');
-    
+
         $('#delete-cookie-home svg').on('click', async function () {
             let
             url = BASE_URL.replace('index.php/', '') + 'prehome/index/setcookie',
             url_base_default = BASE_URL.replace('index.php/', '') + 'prehome/index/webSitesCookie';
 
-            $.ajax({
+            await $.ajax({
                 url: url_base_default,
                 type: "GET",
                 data: {},
@@ -24,7 +24,7 @@ require([
                     url_base_default = data.default_store_url;
                 }
             });
-            
+
             $.ajax({
                 url: url,
                 type: "POST",
@@ -50,7 +50,7 @@ require([
         $('#home-logo-go img').on('click', function(){
             const href = $(this).parent().attr('href');
             const locateCurrent = window.location.href;
-    
+
             if (locateCurrent !== href) {
                 $.cookie('prehomecookie', null);
                 window.location.href = href;
