835c125db40a01160e24fd1da4e95cdb729b947c
Adrián Robledo <arobledo@aventi.co>
2025-06-17 14:14:00 -0500

    Revert "Merge branch 'revert-acb6351d' into 'master'"
    This reverts merge request !151

---
+++ app/code/Aventi/Csp/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright © 2024
+ * Aventi SAS All rights reserved.
+ * See COPYING.txt for license details.
+ * Check module: Cesar Robledo <arobledo@aventi.co>
+ * Magento 2.4.5-p11
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Aventi_Csp',
+    __DIR__
+);
