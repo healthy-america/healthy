3df40eb24edb3dca9fb8bedb5b784957819534c4
Adrián Robledo <arobledo@aventi.co>
2025-06-17 13:30:39 -0500

    Revert "Merge branch 'hotfix/Csp' into 'master'"
    This reverts merge request !150

--- app/code/Aventi/AventiTheme/view/frontend/requirejs-config.js
+++ app/code/Aventi/AventiTheme/view/frontend/requirejs-config.js
@@ -1,9 +1,10 @@
-let config = {
+var config = {
     map: {
         '*': {
-            'Magento_Checkout/template/billing-address/details': 'Aventi_AventiTheme/template/billing-address/details',
-            'Magento_Checkout/template/shipping-address/address-renderer': 'Aventi_AventiTheme/template/shipping-address/address-renderer',
-            'jquery/jquery-migrate': 'Aventi_AventiTheme/js/disable-log-jquery-migrate'
+            'Magento_Checkout/template/billing-address/details':
+                'Aventi_AventiTheme/template/billing-address/details',
+            'Magento_Checkout/template/shipping-address/address-renderer':
+                'Aventi_AventiTheme/template/shipping-address/address-renderer',
         }
     }
 };
